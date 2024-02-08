939b86d415e41fdbef97c91d20f9f98bfc3c88be
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-08 21:57:43 +0100

    :construction: create the useState and the useEffect for the API call  :construction::construction:

---
+++ src/Components/Crypto.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export const Crypto = () => {
+    const [cryptoData, setCryptoData] = useState([])
+
+
+
+
+
+
+  return (
+    <div>
+        <h2>Crypto</h2>
+        <div>
+
+        </div>
+    </div>
+  )
+}
