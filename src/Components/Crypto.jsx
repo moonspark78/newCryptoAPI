5779ff77eee9e31b10d3f8dc864d76e7af4b7bb5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-08 22:57:49 +0100

    :construction: create the useState and the useEffect for the API call  :construction::construction:

--- src/Components/Crypto.jsx
+++ src/Components/Crypto.jsx
@@ -1,7 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export const Crypto = () => {
     const [cryptoData, setCryptoData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            const data = await response.json();
+            console.log(data);
+            setCryptoData(data); 
+        };
+        fetchData();
+    },[])
 
 
 
@@ -11,8 +22,15 @@ export const Crypto = () => {
   return (
     <div>
         <h2>Crypto</h2>
-        <div>
+        <div className='card'>
+            {
+                cryptoData.map(crypto =>(
+                    <div>
+                        <p>{crypto.name}</p>
+                    </div>
 
+                ))
+            }
         </div>
     </div>
   )
